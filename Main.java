a6f1c7302d5dcc06db019c652dc512af4e012437
Mahmoud Helmy <77947371+MaMH20@users.noreply.github.com>
2022-05-11 08:45:14 +0200

    firt commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+
+public  class Main
+{
+    /*
+     * Mein neuer Kommentar
+     */
+    public static void main(String args[])
+    {
+        System.out.println ("Hallo Welt!");
+        
+        int sum = 0;
+        for (int i = 1; i <= 50 ; ++i)
+        {
+            sum += i;
+        }
+        System.out.println(sum);
+        
+        int mul = 1;
+        for (int i = 1; i <= 10 ; ++i)
+        {
+            mul *= i;
+        }
+        System.out.println(mul);
+    }
+}
